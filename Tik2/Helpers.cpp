669376af0e9b83093371fc77c7b6363aee9c4770
Tamep111 <tameplan1939@gmail.com>
2024-01-30 22:19:25 +0600

    Practice 1

---
+++ Tik2/Helpers.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+	int Result = Qadro(2, 3);
+	std::cout << "Result = " << Result << std::endl;
+}
---
+++ Tik2/Helpers.h
@@ -0,0 +1,5 @@
+int Qadro(int a, int b)
+{
+	int c(a + b);
+	return c * c;
+}
