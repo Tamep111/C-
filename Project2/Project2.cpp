1dd2fc17c7710e1683c671d268d160bda0e891aa
Tamep111 <tameplan1939@gmail.com>
2024-04-23 20:35:13 +0600

    new homeworlk

---
+++ Project2/Project2.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+#include <stdint.h>
+#include <iomanip>
+#include <string>
+
+int main()
+{
+	const int N = 150;
+	bool p;
+	std::cout << "select Even = 0, Odd = 1" << '\n';
+	std::cin >> p;
+	std::cout << '\n';
+
+	for (int i = 0; i <= N; i++)
+	{
+		if (i % 2 == p)
+			std::cout << i << '\n';
+	}
+}
