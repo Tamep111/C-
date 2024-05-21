c3d5bbe398afa0072ba401a4732c575da0ee6c10
Tamep111 <tameplan1939@gmail.com>
2024-05-21 23:06:39 +0600

    new homework

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -3,18 +3,40 @@
 #include <stdint.h>
 #include <iomanip>
 #include <string>
+#include <time.h>
+
 
 int main()
 {
-	const int N = 150;
-	bool p;
-	std::cout << "select Even = 0, Odd = 1" << '\n';
-	std::cin >> p;
-	std::cout << '\n';
-
-	for (int i = 0; i <= N; i++)
+	const int size = 5;
+	int i = 0;
+	int j = 0;
+	int array[size][size] =
 	{
-		if (i % 2 == p)
-			std::cout << i << '\n';
+		{(i)+(j),(i)+(++j), (i)+(++j),(i)+(++j),(i)+(++j)},
+		{(++i) + (j = 0),(i)+(++j), (i)+(++j),(i)+(++j), (i)+(++j)},
+		{(++i) + (j = 0),(i)+(++j), (i)+(++j),(i)+(++j), (i)+(++j)},
+		{(++i) + (j = 0),(i)+(++j), (i)+(++j),(i)+(++j), (i)+(++j)},
+		{(++i) + (j = 0),(i)+(++j), (i)+(++j),(i)+(++j), (i)+(++j)}
+	};
+
+	for (int a = 0; a < size; a++)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			std::cout << array[a][i] << " ";
+		}
+		std::cout << '\n';
 	}
+	struct tm buf;
+	time_t t = time(NULL);
+	localtime_s(&buf, &t);
+	
+	int summ = 0;
+	int ind = buf.tm_mday % size;
+	for (j = 0; j < size; j++)
+	{
+		summ = summ + array[ind][j];
+	}
+	std::cout << summ;
 }
