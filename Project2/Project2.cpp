7487feb3aa846a0e1459f74844be2cfd164b3d89
Tamep111 <tameplan1939@gmail.com>
2024-05-23 23:36:00 +0600

    New Homework

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -3,40 +3,55 @@
 #include <stdint.h>
 #include <iomanip>
 #include <string>
-#include <time.h>
 
+class Vector
+{
+private:
+	double a = 0;
+	double b = 0;
+	double a1 = 1;
+	double b1 = 1;
+public:
+	int La = 0, Lb = 0, LV = 0;
+	Vector(double aa, double ab, double aa1, double ab1) : a(aa), b(ab), a1(aa1), b1(ab1)
+	{}
+		void Show()
+		{
+			std::cout << a << "." << b << '\n' << a1 << "." << b1 << '\n' << '\n';
+		}
+		void VLenght()
+		{
+			if (a < a1)
+			{
+				La = a1 - a;
+			}
+			else
+			{ 
+				La = a - a1;
+			}
+			if (b < b1)
+			{
+				Lb = b1 - b;
+			}
+			else
+			{
+				Lb = b - b1;
+			}
+			LV = sqrt(pow(La, 2) + pow(Lb, 2));
+			std::cout << "The Vector lenght is: " << LV;
+		}
+
+};
 
 int main()
 {
-	const int size = 5;
-	int i = 0;
-	int j = 0;
-	int array[size][size] =
-	{
-		{(i)+(j),(i)+(++j), (i)+(++j),(i)+(++j),(i)+(++j)},
-		{(++i) + (j = 0),(i)+(++j), (i)+(++j),(i)+(++j), (i)+(++j)},
-		{(++i) + (j = 0),(i)+(++j), (i)+(++j),(i)+(++j), (i)+(++j)},
-		{(++i) + (j = 0),(i)+(++j), (i)+(++j),(i)+(++j), (i)+(++j)},
-		{(++i) + (j = 0),(i)+(++j), (i)+(++j),(i)+(++j), (i)+(++j)}
-	};
-
-	for (int a = 0; a < size; a++)
-	{
-		for (int i = 0; i < size; i++)
-		{
-			std::cout << array[a][i] << " ";
-		}
-		std::cout << '\n';
-	}
-	struct tm buf;
-	time_t t = time(NULL);
-	localtime_s(&buf, &t);
-	
-	int summ = 0;
-	int ind = buf.tm_mday % size;
-	for (j = 0; j < size; j++)
-	{
-		summ = summ + array[ind][j];
-	}
-	std::cout << summ;
+	int aa, ab, aa1, ab1;
+	std::cout << "Enter initial coordinates of vector, then press Enter" << '\n';
+	std::cin >> aa >> ab;
+	std::cout << '\n' << "Enter the final coordinates of vector, then press Enter" << '\n';
+	std::cin >> aa1 >> ab1;
+	std::cout << '\n' << "Good Job! Vector Coordinates is:" << '\n';
+	Vector v(aa, ab, aa1, ab1);
+	v.Show();
+	v.VLenght();
 }
