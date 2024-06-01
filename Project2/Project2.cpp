3ea8cb249058cbf3de8174cb5f1fcb24fcc58195
Tamep111 <tameplan1939@gmail.com>
2024-06-01 21:39:09 +0600

    New HomeWork

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -1,57 +1,57 @@
 ﻿
-#include <iostream>
-#include <stdint.h>
-#include <iomanip>
-#include <string>
+#include <iostream>  
+#include <algorithm> 
+using namespace std;
 
-class Vector
+class player
 {
-private:
-	double a = 0;
-	double b = 0;
-	double a1 = 1;
-	double b1 = 1;
+	string name;
+	int points = 0;
 public:
-	int La = 0, Lb = 0, LV = 0;
-	Vector(double aa, double ab, double aa1, double ab1) : a(aa), b(ab), a1(aa1), b1(ab1)
-	{}
-		void Show()
-		{
-			std::cout << a << "." << b << '\n' << a1 << "." << b1 << '\n' << '\n';
-		}
-		void VLenght()
-		{
-			if (a < a1)
-			{
-				La = a1 - a;
-			}
-			else
-			{ 
-				La = a - a1;
-			}
-			if (b < b1)
-			{
-				Lb = b1 - b;
-			}
-			else
-			{
-				Lb = b - b1;
-			}
-			LV = sqrt(pow(La, 2) + pow(Lb, 2));
-			std::cout << "The Vector lenght is: " << LV;
-		}
-
+	friend bool point_sorter(const player&, const player&);
+	string get_name()const 
+	{
+		return name; 
+	}
+	int get_points()const 
+	{ 
+		return points; 
+	}
+	void set_name(string n) 
+	{ 
+		name = n; 
+	}
+	void set_points(int n) 
+	{ 
+		points = n; 
+	}
 };
-
-int main()
+bool point_sorter(const player& l, const player& r)
 {
-	int aa, ab, aa1, ab1;
-	std::cout << "Enter initial coordinates of vector, then press Enter" << '\n';
-	std::cin >> aa >> ab;
-	std::cout << '\n' << "Enter the final coordinates of vector, then press Enter" << '\n';
-	std::cin >> aa1 >> ab1;
-	std::cout << '\n' << "Good Job! Vector Coordinates is:" << '\n';
-	Vector v(aa, ab, aa1, ab1);
-	v.Show();
-	v.VLenght();
+	return l.points > r.points;
 }
+int main() {
+	size_t P_numbr;
+	cout << "insert number of players ";
+	cin >> P_numbr;
+
+	player* players = new player[P_numbr];
+	for (size_t i = 0; i < P_numbr; i++)
+	{
+		string name;
+		int point;
+		cout << "Insert name of player " << i+1 << " (name points):";
+		cin >> name >> point;
+		players[i].set_name(name);
+		players[i].set_points(point);
+	}
+
+	sort(players, players + P_numbr, point_sorter);
+	for (size_t i = 0; i < P_numbr; i++)
+	{
+		cout << players[i].get_name() << " " << players[i].get_points() << '\n';
+	}
+
+	delete[] players;
+}
+
