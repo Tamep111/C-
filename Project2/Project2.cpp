dde5e22c95f0d9011b3e3661a50765baa56c9a2e
Tamep111 <tameplan1939@gmail.com>
2024-06-04 22:14:25 +0600

    New Homework

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -9,9 +9,8 @@ class Animals
 public:
 	virtual string Voice()
 	{
-		string V = "Text";
-		std::cout << V;
-		return V;
+		std::cout << "Text";
+		return "Text";
 	}
 };
 class Dog : public Animals
@@ -19,9 +18,8 @@ class Dog : public Animals
 public:
 	string Voice() override
 	{
-		string V = "Woof!\n";
-		std::cout << V;
-		return V;
+		std::cout << "Woof!\n";
+		return "Woof!\n";
 	}
 };
 class Cat : public Animals
@@ -29,9 +27,8 @@ class Cat : public Animals
 public:
 	string Voice() override
 	{
-		string V = "Meow\n";
-		std::cout << V;
-		return V;
+		std::cout << "Meow\n";
+		return "Meow\n";
 	}
 };
 class Cow : public Animals
@@ -39,9 +36,8 @@ class Cow : public Animals
 public:
 	string Voice() override
 	{
-		string V = "Mooo\n";
-		std::cout << V;
-		return V;
+		std::cout << "Mooo\n";
+		return "Mooo\n";
 	}
 };
 class Racoon : public Animals
@@ -49,19 +45,46 @@ class Racoon : public Animals
 public:
 	string Voice() override
 	{
-		string V = "FRFRFRFR\n";
-		std::cout << V;
-		return V;
+		std::cout << "FRFRFRFR\n";
+		return "FRFRFRFR\n";
+	}
+};
+class Fox : public Animals
+{
+public:
+	string Voice() override
+	{
+		int answ;
+		std::cout << "Do you want to know, what does the Fox say? (yes - 1 /no - 0) - ";
+			std::cin >> answ;
+			if (answ == 1)
+			{
+				std::cout << "Tin din din din din din trin rin rin rin rin Rapapapapapau";
+				return "Tin din din din din din trin rin rin rin rin Rapapapapapau";
+			}
+			else
+			{
+				std::cout << "Anyway Tin din din din din din trin rin rin rin rin Rapapapapapau";
+				return "Anyway Tin din din din din din trin rin rin rin rin Rapapapapapau";
+			}
 	}
 };
 int main()
 {
-	Dog* dog = new Dog;
-	Cat* cat = new Cat;
-	Cow* cow = new Cow;
-	Racoon* rac = new Racoon;
-	dog->Voice();
-	cat->Voice();
-	cow->Voice();
-	rac->Voice();
+	const int arsize = 5;
+	Animals* array[arsize];
+	array[0] = new Dog();
+	array[1] = new Cat();
+	array[2] = new Cow();
+	array[3] = new Racoon();
+	array[4] = new Fox();
+	for (int i = 0; i < arsize; ++i)
+	{
+		array[i]->Voice();
+	}
+	for (int i = 0; i < arsize; ++i) {
+		delete array[i];
+	}
+
+	return 0;
 }
